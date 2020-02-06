4816bfb8cebdf305c9b385bce76e9270ecf53e14
anjako <anjako@ttu.ee>
2020-02-06 21:02:36 +0200

    initialize project

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Client from "./Client";
 
 import './App.css';
 import Home from "./components/Home";
